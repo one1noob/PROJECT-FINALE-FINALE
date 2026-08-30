a6137feccc0b838a150515a1898c655dcb5dbb2d
one1noob <azalifan123@gmail.com>
2026-08-30 20:50:33 +0200

    the biggest change yet
    weirdly its the smallest written part but its the one who took me the most time

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
-var modal = document.getElementById('id01');
+window.onload = function () {
+  let cartList = document.getElementById('cart-items');
+  let buttons = document.querySelectorAll('.add-cart');
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', function (event) {
+      let button = event.currentTarget;
+      let card = button.parentElement;
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+      let img = card.querySelector('img');
+      let name = img.alt;
+      let select = card.querySelector('select');
+      let size = select.value;
+      let li = document.createElement('li');
+      let text = document.createElement('span');
+      text.textContent = name + ' - Size: ' + size;
+      let del = document.createElement('button');
+      del.textContent = 'Delete';
+      del.addEventListener('click', function () {
+        del.parentElement.remove();
+      });
+      li.appendChild(text);
+      li.appendChild(del);
+      cartList.appendChild(li);
+    });
+  }
+};
